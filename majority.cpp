917c75f4623fa7707eddd8b0a37d329e714a87b2
Roshni <62755400+belhyto@users.noreply.github.com>
2024-06-24 11:49:50 +0530

    Create majority.cpp

---
+++ majority.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public: 
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int, int> hash;
+        int res=0;
+        int majority=0;
+        for(int n:nums)
+        {
+            hash[n]++;
+            if(hash[n]>majority)
+            {
+             res=n;
+             majority=hash[n];
+            }
+        }
+        return res;        
+
+    }
+};
